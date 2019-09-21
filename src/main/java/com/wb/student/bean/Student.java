9f7d28dea66b266f93e6e845a2db87d934552e10
gongDongXiang <2209623749@qq.com>
2019-09-21 12:43:24 +0800

    第一次提交

---
+++ src/main/java/com/wb/student/bean/Student.java
@@ -0,0 +1,63 @@
+package com.wb.student.bean;
+
+public class Student {
+    private Integer id;
+
+    private String name;
+
+    private Integer gender;
+
+    private String address;
+
+    private String email;
+
+    private Integer gradeId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(Integer gradeId) {
+        this.gradeId = gradeId;
+    }
+}
